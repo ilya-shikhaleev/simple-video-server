fa2035991ac41b6ba90419ee28566c971fdba102
ilya.shikhaleev <ilya.shikhaleev@gmail.com>
2018-01-30 23:07:01 +0300

    Добавлено логирование запуска и всех запросов в файл

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,13 +3,26 @@ package handlers
 import (
 	"github.com/gorilla/mux"
 	"net/http"
+	log "github.com/sirupsen/logrus"
 )
 
 // Router register necessary routes and returns an instance of a router.
-func Router() *mux.Router {
+func Router() http.Handler {
 	r := mux.NewRouter()
 	s := r.PathPrefix("/api/v1").Subrouter()
 	s.HandleFunc("/list", list).Methods(http.MethodGet)
 	s.HandleFunc("/video/d290f1ee-6c54-4b01-90e6-d701748f0851", video).Methods(http.MethodGet)
-	return r
+	return logRequest(r)
+}
+
+func logRequest(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.WithFields(log.Fields{
+			"method":     r.Method,
+			"url":        r.URL,
+			"remoteAddr": r.RemoteAddr,
+			"userAgent":  r.UserAgent(),
+		}).Info("got new request")
+		h.ServeHTTP(w, r)
+	})
 }
--- main.go
+++ main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"net/http"
 	"github.com/ilya-shikhaleev/simple-video-server/handlers"
-	"fmt"
+	log "github.com/sirupsen/logrus"
+	"os"
 )
 
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+}
 func main() {
+	file, err := os.OpenFile("server.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err == nil {
+		log.SetOutput(file)
+	}
+	defer file.Close()
+
+	serverUrl := ":8000"
+	log.WithFields(log.Fields{
+		"serverUrl": serverUrl,
+	}).Info("starting the server")
 	router := handlers.Router()
-	fmt.Println(http.ListenAndServe(":8000", router))
+	log.Info("the server is ready to serve requests")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
